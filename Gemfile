e15e3d64d2605d43c2bd2ea6ad0fa2fa297ec3ba
Gabe Nielsen <gabenielsen7@gmail.com>
2022-07-08 16:56:21 -0700

    add RAKE algorithm for text analysis

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'httparty'
 gem 'chartkick'
 gem 'hightop'
+gem 'rake_text'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :game
+  require 'rake_text'
+
+  # def write_file(hash)
+  #   CSV.open("data.csv", 'w', headers: hash.keys) do |csv|
+  #     csv << hash.values
+  #   end
+  # end
 
   # This pair of fuctions gathers all the API data that we need for the searched game. The first function exists to grab the game ID with the search function of the API. The second function takes the game_id and gets the first 100 reviews (called comments) for the game. It then loops through each page, grabbing the actual text comments, until all the reviews are parsed through.
 
@@ -10,27 +17,25 @@ class Review < ApplicationRecord
   end
 
   def self.get_comments(name)
-    review_words = []
     get_game_id(name)
     response = HTTParty.get('https://boardgamegeek.com/xmlapi2/thing?&id=' + @game_id + '&ratingcomments=1&pagesize=100&page=1')
 
     # If every review is desired, uncomment and replace the number with 'pages' (beware: has the potential to be in the hundreds)
     # total_reviews = response['items']['item']['comments']['totalitems']
     # pages = total_reviews.to_i / 100
-    20.times do |n|
+    text = ""
+    5.times do |n|
       review_response = HTTParty.get('https://boardgamegeek.com/xmlapi2/thing?&id=' + @game_id + "&ratingcomments=1&pagesize=100&page=#{n}")
-      review_response['items']['item']['comments']['comment'].each do |c|
+      review_response['items']['item']['comments']['comment'].each do |c|        
         if c["value"] != ""
-          c['value'].split(' ').each do |word|
-            word_cut = word.gsub(/[^A-Za-z]/, '').downcase
-            review_words.push(word_cut)
-          end
+          text += " #{c['value']}"
         end
       end
     end
-    p review_words.top(100)
+    rake = RakeText.new
+    keywords = rake.analyse text, RakeText.SMART
+    CSV.open("data.csv", 'w') {|csv| keywords.to_a.each {|elem| csv << elem} }
     return response
   end
-
 
 end
