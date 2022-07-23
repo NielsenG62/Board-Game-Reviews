2d731208cf10c92d3367a4496995106fc20b2f08
Gabe Nielsen <gabenielsen7@gmail.com>
2022-07-22 17:44:55 -0700

    WIP: trying to get routes set up correctly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
   def index
     render :index
   end
+
+  def search
+    @name = Review.filter_comments(params[:name])
+    render :search
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/index' => 'home#index'
+  post '/index' => 'home#search'
   root to: "home#index"
+
 end
